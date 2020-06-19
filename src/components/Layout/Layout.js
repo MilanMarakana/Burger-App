68fee81901db18662cbd8bfc5d671dffdccbcc54
Milan Patel <milan.marakana7@gmail.com>
2020-06-19 19:36:30 +0200

    Update container and apply style to App

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import Aux from '../../hoc/Auxiliary';
+import Classes from './Layout.module.css';
 
 
 const layout = (props) => (
     <Aux>
     <div>Toolbar, Sidebar and Backdoor</div>
-    <main>
+    <main className={Classes.Content}>
         {props.children}
     </main>
     </Aux>
