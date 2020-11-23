d867e0a16ff19a24e3c256681a7ec01c39b37694
Milan Patel <milan.marakana7@gmail.com>
2020-11-23 23:38:53 +0100

    Improved burger state using Redux and React-redux

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import axios from "../../../axios-orders";
+import { connect } from "react-redux";
 
 import Button from "../../../components/Burger/UI/Button/Button";
 import classes from "./ContactData.module.css";
@@ -45,7 +46,8 @@ class ContactData extends Component {
         validation: {
           required: true,
           minLength: 5,
-          maxLength: 5
+          maxLength: 5,
+          isNumeric: true
         },
         valid: false,
         touched: false
@@ -71,7 +73,8 @@ class ContactData extends Component {
         },
         value: "",
         validation: {
-          required: true
+          required: true,
+          isEmail: true
         },
         valid: false,
         touched: false
@@ -101,9 +104,9 @@ class ContactData extends Component {
       formData[formElementIndentifier] = this.state.orderForm[formElementIndentifier].value;
     }
     const order = {
-      ingredients: this.props.ingredients,
+      ingredients: this.props.ings,
       price: this.props.price,
-      orderData : formData
+      orderData: formData,
       // customer: {
       //   name: "Milan patel",
       //   address: {
@@ -128,21 +131,34 @@ class ContactData extends Component {
  
   checkValidity(value, rules) {
     let isValid = true;
-
-    if(rules.required) { 
-      isValid = value.trim() !== '' && isValid;
+    if (!rules) {
+        return true;
+    }
+    
+    if (rules.required) {
+        isValid = value.trim() !== '' && isValid;
     }
 
-    if(rules.minLength) {
-      isValid = value.length >= rules.minLength && isValid;
+    if (rules.minLength) {
+        isValid = value.length >= rules.minLength && isValid
     }
 
-    if(rules.maxLength) { 
-      isValid = value.length <= rules.maxLength && isValid;
+    if (rules.maxLength) {
+        isValid = value.length <= rules.maxLength && isValid
+    }
+
+    if (rules.isEmail) {
+        const pattern = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/;
+        isValid = pattern.test(value) && isValid
+    }
+
+    if (rules.isNumeric) {
+        const pattern = /^\d+$/;
+        isValid = pattern.test(value) && isValid
     }
 
     return isValid;
-  }
+}
   
   inputChangeHandler = (event, inputIdentifier) => {
     const updatedOrderForm = {
@@ -205,4 +221,11 @@ class ContactData extends Component {
   }
 }
 
-export default ContactData;
+const mapStateToProps = (state) => {
+  return {
+    ings: state.ingredients,
+    price: state.totalPrice,
+  };
+};
+
+export default connect(mapStateToProps)(ContactData);
--- src/index.js
+++ src/index.js
@@ -1,16 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from "react-redux";
+import { createStore } from "redux";
+
 import './index.css';
 import App from './App';
+import reducer from "./store/reducer";
 import * as serviceWorker from './serviceWorker';
 import { BrowserRouter } from "react-router-dom";
 
+const store = createStore(reducer);
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+      <Provider store={store}>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </Provider>
   </React.StrictMode>,
   document.getElementById("root")
 );
